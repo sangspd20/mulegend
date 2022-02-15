b48c398c191cc16d3ccc6bb7816b243e0f38bb52
F88\sangnt3 <sangnt3@f88.vn>
2022-02-15 20:55:29 +0700

    update

--- js/custom.js
+++ js/custom.js
@@ -16,6 +16,7 @@ btn.on("click", function (e) {
   $("html, body").animate({ scrollTop: 0 }, "300");
 });
 
+// scroll item click
 $(".scroll-item").click(function () {
   var scrollList = $(".scroll-list");
   var liList = scrollList.find(".scroll-item");
@@ -26,3 +27,24 @@ $(".scroll-item").click(function () {
 
   $(this).addClass("active");
 });
+
+// scroll to zone active link
+const sections = $(".event-cont").find(".event-item");
+const navLi = $(".scroll-list").find(".scroll-item");
+window.onscroll = () => {
+  var current = "";
+
+  sections.each(function (index, section) {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.each(function (index, li) {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
